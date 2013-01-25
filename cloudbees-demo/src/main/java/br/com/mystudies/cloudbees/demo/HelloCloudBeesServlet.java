478dc6ce0d3be5d41f5e86246e7105bcae6ad4df
Robson <Robson@Robson-PC>
2013-01-24 22:38:30 -0200

    temp

--- cloudbees-demo/src/main/java/br/com/mystudies/cloudbees/demo/HelloCloudBeesServlet.java
+++ cloudbees-demo/src/main/java/br/com/mystudies/cloudbees/demo/HelloCloudBeesServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloCloudBeesServlet extends HttpServlet {
 
 	
+	
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
